3ee980b3966d620b02bc845e76fd555c38d06b05
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2022-12-13 17:54:26 +0530

    Added: Uc6StoreDic added

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -10,3 +10,10 @@ a3=$((c+a/b))
 
 a4=$((a%b+c))
 
+declare -A Compute
+Compute[a1]=$a1
+Compute[a2]=$a2
+Compute[a3]=$a3
+Compute[a4]=$a4
+
+arr=(${Compute[a1]} ${Compute[a2]} ${Compute[a3]} ${Compute[a4]})
